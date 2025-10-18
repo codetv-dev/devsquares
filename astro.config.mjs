a4c3ae1a48333e95f1dc9aae0cd8074c7cb5d9f5
Jason Lengstorf <jason@codetv.dev>
2025-10-17 21:48:43 -0400

    feat: add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig, envField } from 'astro/config';
 import react from '@astrojs/react';
 import clerk from '@clerk/astro';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	env: {
-		schema: {
-			CONVEX_URL: envField.string({
-				context: 'client',
-				access: 'public',
-			}),
-		},
+  output: 'server',
+
+  env: {
+      schema: {
+          CONVEX_URL: envField.string({
+              context: 'client',
+              access: 'public',
+          }),
+      },
 	},
-	integrations: [react(), clerk()],
+
+  integrations: [react(), clerk()],
+  adapter: netlify(),
 });
